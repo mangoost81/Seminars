fb58d7d95ac7b3cc76d5e74d9a6966e3b33685ca
Dmyrix <mangoost81@gmail.com>
2023-12-12 19:55:04 +0300

    add small programm

--- Seminar_1/Program.cs
+++ Seminar_1/Program.cs
@@ -1,2 +1,14 @@
-﻿vczxvxc
-bxcbxbc
+﻿Console.WriteLine("ВВедите число, квадрат которого хотите получить. ");
+
+
+
+//Преобразование строки в число (ввод всегда происходит строкой)
+int num = Convert.ToInt32(Console.ReadLine());  
+
+Console.WriteLine($"Квадрат числа {num} равен {num*num}");
+
+//Вывод квадрата введенного числа
+//Console.WriteLine(num*num);  
+
+
+
